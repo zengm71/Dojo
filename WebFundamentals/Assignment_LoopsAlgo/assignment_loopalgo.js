98e958f82493dd64565e7e8ae5c830bf1e869136
mzeng-moodys <50420796+mzeng-moodys@users.noreply.github.com>
2021-06-21 22:06:42 -0700

    loopalgo

---
+++ WebFundamentals/Assignment_LoopsAlgo/assignment_loopalgo.js
@@ -0,0 +1,12 @@
+for(var i = 1; i < 20; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+var s = 0
+for(var i = 1; i <=5; i++){
+    console.log('Num:', i)
+    s = s + i
+    console.log('Sum:', s)
+}

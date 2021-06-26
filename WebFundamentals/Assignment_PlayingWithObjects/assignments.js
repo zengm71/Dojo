5fb587adac303184a71117e764ee9c053fc46810
MichaelZeng <zengm71@berkeley.edu>
2021-06-26 16:21:52 -0700

    added playwithobjects

---
+++ WebFundamentals/Assignment_PlayingWithObjects/assignments.js
@@ -0,0 +1,5 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+for (let index = 0; index < users.length; index++) {
+    console.log(users[index].name, '-', users[index].age)
+}

802e2dc118a28d62cb8faf4167ca7e264233aafe
MichaelZeng <zengm71@berkeley.edu>
2021-07-24 19:13:44 -0700

    get generate working

--- Python/django/django_intro/random_word_generator/rwg/views.py
+++ Python/django/django_intro/random_word_generator/rwg/views.py
@@ -6,6 +6,7 @@ from django.utils.crypto import get_random_string
 
 # Create your views here.
 def display(request):
+
     print('here in function display')
     print(request.session['result'])
     context = {
@@ -16,6 +17,7 @@ def display(request):
     return render(request, 'index.html', context)
 
 def init(request):
+    
     request.session.flush()
     request.session['result'] = {
         'string': get_random_string(length=14),
@@ -26,17 +28,38 @@ def init(request):
     return redirect('/random_word')
 
 def generate(request):
+    if request.method == 'GET':
+        print('redirecting')
+        return redirect('/')
     print('here in function generate')
     print(request.session['result'])
     request.session['result']['string'] = get_random_string(length=14)
     request.session['result']['counter'] += 1
+    request.session.modified = True
     print(request.session['result'])
-
     return redirect('/random_word')
+    # context = {
+    #     'string': request.session['result']['string'],
+    #     'counter': request.session['result']['counter']
+    # }
+    # print(context)
+    # return render(request, 'index.html', context)
 
 def reset(request):
+    if request.method == 'GET':
+        print('redirecting')
+        return redirect('/')
     print('here in function reset')
+    request.session['result']['string'] = get_random_string(length=14)
     request.session['result']['counter'] = 0
-    print(request.session['result'])
+    request.session.modified = True
 
+    print(request.session['result'])
     return redirect('/random_word')
+
+    # context = {
+    #     'string': request.session['result']['string'],
+    #     'counter': request.session['result']['counter']
+    # }
+    # print(context)
+    # return render(request, 'index.html', context)

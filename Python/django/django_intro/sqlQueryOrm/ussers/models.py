3af797a470dc82c0c974200f8848f0e1499d91dd
MichaelZeng <zengm71@berkeley.edu>
2021-07-27 15:47:02 -0700

    finished SQL ORM

---
+++ Python/django/django_intro/sqlQueryOrm/ussers/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
+
+# from ussers.models import Wizard
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

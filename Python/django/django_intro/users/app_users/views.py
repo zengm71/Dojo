65b7260f0b5579b461069a8a29a5145576444a9a
MichaelZeng <zengm71@berkeley.edu>
2021-07-27 17:50:15 -0700

    adding users with templatse

--- Python/django/django_intro/users/app_users/views.py
+++ Python/django/django_intro/users/app_users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from app_users.models import Users
 
 # Create your views here.
+def index(request):
+    context = {
+    	"all_users": Users.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def addUser(request):
+    print(request.POST)
+    Users.objects.create(first_name = request.POST['first_name'], \
+        last_name = request.POST['first_name'], \
+        email_address = request.POST['email'], \
+        age = request.POST['age'])
+    return redirect('/')

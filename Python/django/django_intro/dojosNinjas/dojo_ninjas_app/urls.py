c80b2a2fdcf4607a1df9da1f47d439b57c0d1942
zengm71 <zengm71@berkeley.edu>
2021-07-29 23:13:50 -0700

    finished ninja dojos template

---
+++ Python/django/django_intro/dojosNinjas/dojo_ninjas_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('addDojo', views.addDojo),
+    path('addNinja', views.addNinja)
+]
--- Python/django/django_intro/dojosNinjas/dojo_ninjas_app/views.py
+++ Python/django/django_intro/dojosNinjas/dojo_ninjas_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from dojo_ninjas_app.models import Dojo, Ninja
 
 # Create your views here.
+def index(request):
+    context = {
+    	"all_dojos": Dojo.objects.all(), 
+        "all_ninjas": Ninja.objects.all()
+
+    }
+    return render(request, 'index.html', context)
+
+def addDojo(request):
+    print(request.POST)
+    Dojo.objects.create(name = request.POST['name'], \
+        city = request.POST['city'], \
+        state = request.POST['state'], \
+        desc = 'nothing')
+    return redirect('/')
+
+def addNinja(request):
+    print(request.POST)
+    Ninja.objects.create(first_name = request.POST['first_name'], \
+        last_name = request.POST['last_name'], \
+        dojo = Dojo.objects.filter(name = request.POST['dojo'])[0])
+    return redirect('/')

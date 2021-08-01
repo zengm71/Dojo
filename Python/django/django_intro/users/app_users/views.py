a8e780b54bda74ebd60c825a79cc64e651ffa72a
MichaelZeng <zengm71@berkeley.edu>
2021-07-31 21:30:29 -0700

    edit typo

--- Python/django/django_intro/users/app_users/views.py
+++ Python/django/django_intro/users/app_users/views.py
@@ -11,7 +11,7 @@ def index(request):
 def addUser(request):
     print(request.POST)
     Users.objects.create(first_name = request.POST['first_name'], \
-        last_name = request.POST['first_name'], \
+        last_name = request.POST['last_name'], \
         email_address = request.POST['email'], \
         age = request.POST['age'])
     return redirect('/')
